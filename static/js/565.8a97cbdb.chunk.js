2022c483ec74aff966adfffd3751c4363a499b4d
VlikToldo <108451651+VlikToldo@users.noreply.github.com>
2023-05-24 16:08:49 +0300

    Updates

---
+++ static/js/565.8a97cbdb.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkhayloft_menu=self.webpackChunkhayloft_menu||[]).push([[565],{7565:function(e,n,u){u.r(n);var t=u(184);n.default=function(){return(0,t.jsx)("div",{children:"Bar List"})}}}]);
+//# sourceMappingURL=565.8a97cbdb.chunk.js.map
---
+++ static/js/680.434c0f6b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkhayloft_menu=self.webpackChunkhayloft_menu||[]).push([[680],{680:function(e,n,u){u.r(n);var t=u(184);n.default=function(){return(0,t.jsx)("div",{children:"HomePage"})}}}]);
+//# sourceMappingURL=680.434c0f6b.chunk.js.map
