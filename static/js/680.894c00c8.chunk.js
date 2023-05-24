db170eaf4d584afafc5fa4170f26b0ac949c93a1
VlikToldo <108451651+VlikToldo@users.noreply.github.com>
2023-05-24 16:47:42 +0300

    Updates

---
+++ static/js/680.894c00c8.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkhayloft_menu=self.webpackChunkhayloft_menu||[]).push([[680],{680:function(e,n,u){u.r(n);var t=u(184);n.default=function(){return(0,t.jsx)("div",{children:"HomePage h"})}}}]);
+//# sourceMappingURL=680.894c00c8.chunk.js.map
