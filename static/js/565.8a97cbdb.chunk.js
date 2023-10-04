970d4102e2100ce4bc2f709ad2778fbf49bd4b72
VlikToldo <108451651+VlikToldo@users.noreply.github.com>
2023-10-04 11:21:05 +0000

    Deploying to gh-pages from @ VlikToldo/hayloft-menu@959279bafff2a935eaa5928825314923abb28acd 🚀

--- static/js/565.8a97cbdb.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkhayloft_menu=self.webpackChunkhayloft_menu||[]).push([[565],{7565:function(e,n,u){u.r(n);var t=u(184);n.default=function(){return(0,t.jsx)("div",{children:"Bar List"})}}}]);
-//# sourceMappingURL=565.8a97cbdb.chunk.js.map
